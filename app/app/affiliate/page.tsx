515533e9a4c95704e0e4acd60f6e3e678edcf283
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-25 01:48:38 -0500

    Delete page.tsx

--- app/app/affiliate/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AffiliatePage() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Affiliate Program Working</h1>
-      <p>If you can see this, routing is fixed.</p>
-      <p><a href="/">Back Home</a></p>
-    </main>
-  );
-}
