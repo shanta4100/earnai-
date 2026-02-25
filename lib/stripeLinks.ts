65fe67ee78e734c3483edcde3c75296e9068b1e9
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-24 22:46:01 -0500

    Create stripeLinks.ts

---
+++ lib/stripeLinks.ts
@@ -0,0 +1,5 @@
+export const STRIPE_LINKS = {
+  basic: "https://buy.stripe.com/bJe8wO1Mn9HAe2Gfk557W03",
+  standard: "https://buy.stripe.com/fZufZggHh4ng0bQ4Fr57W04",
+  pro: "https://buy.stripe.com/3cI4gydv53jc2jY2xj57W05",
+} as const;
