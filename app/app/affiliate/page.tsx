03129f14a1bfb30784ad4d19114e31a4686b2128
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-25 01:15:46 -0500

    Create page.tsx

---
+++ app/app/affiliate/page.tsx
@@ -0,0 +1,9 @@
+export default function AffiliatePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Affiliate Program Working</h1>
+      <p>If you can see this, routing is fixed.</p>
+      <p><a href="/">Back Home</a></p>
+    </main>
+  );
+}
