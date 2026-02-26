c13fd03c5b310e218ecb78404cb0b902e455d041
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-26 02:33:35 -0500

    Delete stripeLinks.ts

--- lib/stripeLinks.ts
+++
@@ -1,5 +0,0 @@
-export const STRIPE_LINKS = {
-  basic: "https://buy.stripe.com/bJe8wO1Mn9HAe2Gfk557W03",
-  standard: "https://buy.stripe.com/fZufZggHh4ng0bQ4Fr57W04",
-  pro: "https://buy.stripe.com/3cI4gydv53jc2jY2xj57W05",
-} as const;
